b2041639fe9d2815b6a3796a2577727b300caa51
msc-dart <matteo.s.corrado.26@dartmouth.edu>
2025-10-08 23:19:20 -0400

    Stop tracking test/ per .gitignore

--- test/app.py
+++
@@ -1,6 +0,0 @@
-from bosdyn.client import create_standard_sdk
-def main():
-    sdk = create_standard_sdk("my-spot-app")
-    print("SDK imported, app skeleton OK")
-if __name__ == "__main__":
-    main()
